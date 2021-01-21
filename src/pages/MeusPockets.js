005c2a1029744b22c1e44bddf63108f61b0829f8
FlaviaCamila <flavia-camila12@hotmail.com>
2021-01-21 02:49:36 -0300

    Página Meus Pockets

---
+++ src/pages/MeusPockets.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+function MeusPockets(){
+    return(
+        <>
+        <h1> Meus Pockets </h1>
+        <ul>
+            <li>Pocket 01</li>
+            <li>Pocket 02</li>
+            <li>Pocket 03</li>
+            <li>Pocket 04</li>
+            <li>Pocket 05</li>
+            <li>Pocket 06</li>
+            <li>Pocket 07</li>
+            <li>Pocket 08</li>
+            <li>Pocket 09</li>
+            <li>Pocket 10</li>
+        </ul>
+    </>
+    )
+}
+
+export default MeusPockets;
