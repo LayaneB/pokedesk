2098627f9c63e287aef0eb801ecb57deaa1c3061
Layane Bastos <LayaneB>
2021-01-22 15:18:26 -0300

    Atualizando o App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import SearchBar from './components/layout/SearchBar';
+import Dashboard from './components/layout/Dashboard';
+import backgroundImage from './backgroundpokenoob.png';
+
+import ReactDom from "react-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar/>
-        <SearchBar/>
-      </div>
+       
+        <div className="App" style={{background:`url(${backgroundImage})`}}>
+          <NavBar/>
+          <SearchBar/>
+          <div className="container">
+            <Dashboard></Dashboard>
+          </div>
+        </div>
+
+
+
     );
   }
 }
