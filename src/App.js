05460730dd2f69067d0cc838c33754f507976c63
PriMelo <priscilamelo.martins@gmail.com>
2021-01-21 22:23:39 -0300

    att NavBar e searchBar App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from './components/layout/NavBar';
+import SearchBar from './components/layout/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         hellow beninass
-         anote!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -4,9 +4,11 @@ import styled from "styled-components";
 export default class SearchBar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <div className="container">                
+                <form className="form-inline my-2 my-lg-3">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>                  
+                </form> 
+            </div>            
         )
     }
 }
