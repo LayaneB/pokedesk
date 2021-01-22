64aa98383b4f888934dc5546d4529e3c857b1726
PriMelo <priscilamelo.martins@gmail.com>
2021-01-21 22:26:46 -0300

    att NavBar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -4,9 +4,32 @@ import styled from 'styled-components';
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="container">
+                <div className="row" id="titulo1">
+                    <div classeName="col-md-12">
+                        <h3>Capture seus Pokémons!</h3>
+                    </div>                
+                </div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" >POKENOOB</a>
+                    <button className="navbar-toggler" type="button" dataToggle="collapse" dataTarget="#navbarSupportedContent" ariaControls="navbarSupportedContent" ariaExpanded="false" ariaLabel="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+              
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Todos Pokémons</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Minha Pokébola</a>
+                            </li>                    
+                        </ul>                        
+                    </div>              
+                </nav>
             </div>
-        )
+        );
     }
 }
+
+            
